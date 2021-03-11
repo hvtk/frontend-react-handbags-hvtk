4493a4fcf226d48b8f80f7613bb46851c83a870b
hvtk <hvtk1968@gmail.com>
2021-03-11 16:33:01 +0100

    first creation buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 function App() {
   return (
+      <>
       <h1>Handbags & Purses</h1>
+      <button type="button"> to the collection </button>
+      <button type="button"> shop all bags </button>
+      <button type="button"> pre-orders </button>
+      </>
   );
 }
 
