7e00e10435efea8da71eb849fb554be9fe1c620a
hvtk <hvtk1968@gmail.com>
2021-03-12 11:25:15 +0100

    button first stap without disable

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
       <>
       <h1>Handbags & Purses</h1>
-      <button type="button"> to the collection </button>
-      <button type="button"> shop all bags </button>
-      <button type="button"> pre-orders </button>
+      <nav>
+      <button type="button" onClick={() => console.log("to the collection")}> to the collection </button>
+      <button type="button" onClick={() => console.log("shop all bags")} > shop all bags </button>
+      <button type="button" onClick={() => console.log("pre-orders")}  > pre-orders </button>
+      </nav>
       </>
   );
 }
